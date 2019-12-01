a19b76ea9352cbfbdf78c9321d298783dcb4d97c
Arpan Bag <arpanbag001@gmail.com>
2019-12-01 22:44:39 +0530

    Merge Sort - Implemented full functionality

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,6 +5,7 @@ import com.company.recursion.Fibonacci;
 import com.company.recursion.StringReverser;
 import com.company.sorting.BubbleSort;
 import com.company.sorting.InsertionSort;
+import com.company.sorting.MergeSort;
 import com.company.sorting.SelectionSort;
 
 import java.util.Arrays;
@@ -13,6 +14,7 @@ public class Main {
 
     public static void main(String[] args) {
 
-        System.out.println(Arrays.toString(InsertionSort.doInsertionSort(new int[]{2, 8, 1, 21, 92, -23, -2, 0, 0, -2})));
+        System.out.println(Arrays.toString(MergeSort.doMergeSort(new int[]{2, 8, 1, 21, 92, -23, -2, 0, 0, -2})));
+        //System.out.println(Arrays.toString(MergeSort.merge(new int[]{2, 8, 1, 1, 92, -23, -2, 0, 0, -2}, 3)));
     }
 }
---
+++ src/com/company/sorting/MergeSort.java
@@ -0,0 +1,40 @@
+package com.company.sorting;
+
+import java.util.Arrays;
+
+public class MergeSort {
+    public static int[] doMergeSort(int[] arrayToSort) {
+        return divide(arrayToSort);
+    }
+
+    private static int[] divide(int[] arr) {
+        if (arr.length == 1)
+            return arr;
+        int mid = arr.length / 2;
+        return sortAndMarge(
+                divide(Arrays.copyOfRange(arr, 0, mid)),
+                divide(Arrays.copyOfRange(arr, mid, arr.length))
+        );
+    }
+
+    public static int[] sortAndMarge(int[] arr1, int[] arr2) {
+        int[] mergedArray = new int[arr1.length + arr2.length];
+        for (int i = 0, j = 0, m = 0; i < arr1.length || j < arr2.length; ) {
+            if (i >= arr1.length) {         //If first array is complete, add from second array
+                mergedArray[m] = arr2[j];
+                j++;
+            } else if (j >= arr2.length) {  //If second array is complete, add from first array
+                mergedArray[m] = arr1[i];
+                i++;
+            } else if (arr1[i] < arr2[j]) { //If first array item is small, add that
+                mergedArray[m] = arr1[i];
+                i++;
+            } else {
+                mergedArray[m] = arr2[j];   //Else, add second array item
+                j++;
+            }
+            m++;
+        }
+        return mergedArray;
+    }
+}

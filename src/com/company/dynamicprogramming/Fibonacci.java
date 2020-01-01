b5c97ddd66b6d990123474ebfdbbc61bab13d5d6
Arpan Bag <arpanbag001@gmail.com>
2020-01-01 14:51:18 +0530

    Dynamic programming - Fibonacci series - Minor changes

--- src/com/company/dynamicprogramming/Fibonacci.java
+++ src/com/company/dynamicprogramming/Fibonacci.java
@@ -3,7 +3,7 @@ package com.company.dynamicprogramming;
 public class Fibonacci {
 
     public static int numberOfOperations;
-    private static int[] dynamicProgrammingDataStore;
+    private static int[] dynamicProgrammingMemo;
 
 
     //Iterative approach
@@ -45,20 +45,20 @@ public class Fibonacci {
     //Dynamic programming
     public static int fibonacciRecursiveWithDynamicProgramming(int index) {
 
-        if (dynamicProgrammingDataStore == null)
-            dynamicProgrammingDataStore = new int[index + 1];   // Doing +1 coz array is staring with 0 index
+        if (dynamicProgrammingMemo == null)
+            dynamicProgrammingMemo = new int[index + 1];   // Doing +1 coz array is staring with 0 index
 
         //Seed values
         if (index <= 1) return 0;
         else if (index == 2) return 1;
 
-        if (dynamicProgrammingDataStore[index] != 0)        // 0 denotes uninitialized here.
-            return dynamicProgrammingDataStore[index];
+        if (dynamicProgrammingMemo[index] != 0)        // 0 denotes uninitialized here.
+            return dynamicProgrammingMemo[index];
 
         numberOfOperations++;   //For complexity analysis
 
         int currNum = fibonacciRecursiveWithDynamicProgramming(index - 2) + fibonacciRecursiveWithDynamicProgramming(index - 1);
-        dynamicProgrammingDataStore[index] = currNum;
+        dynamicProgrammingMemo[index] = currNum;
         return currNum;
     }
 }
